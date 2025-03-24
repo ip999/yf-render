ba4bd1cd90630a06cc57f40816602c388489cca5
I P <porteous@gmail.com>
2025-03-24 14:33:51 +0000

    un break stuff

--- app.py
+++ app.py
@@ -1,40 +1,43 @@
 import yfinance as yf
 from flask import Flask, request, jsonify
-import redis
 import json
+import pkg_resources
 
 app = Flask(__name__)
-cache = redis.Redis(host='red-ci9i4g59aq02iht9og6g', port=6379)
-redis_ttl = 900
 
 @app.route('/')
 def info():
     ticker = request.args.get('ticker')
-    stock = yf.Ticker(ticker)
-    info = stock.info
-    return jsonify(info)
+    if not ticker:
+        return jsonify({"error": "Ticker symbol is required"}), 400
+    try:
+        stock = yf.Ticker(ticker)
+        info = stock.info
+        return jsonify(info)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/news')
 def news():
     ticker = request.args.get('ticker')
-    stock = yf.Ticker(ticker)
-    news = stock.news
-    return jsonify(news)
+    if not ticker:
+        return jsonify({"error": "Ticker symbol is required"}), 400
+    try:
+        stock = yf.Ticker(ticker)
+        news = stock.news
+        return jsonify(news)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/cached')
-def cached():
-    ticker = request.args.get('ticker')
-    cached_info = cache.get(ticker)
-    if cached_info is not None:
-        info = json.loads(cached_info)
-        # info['x-cache'] = True
-    else:
-        try: 
-            stock = yf.Ticker(ticker)
-            cache.set(ticker, json.dumps(stock.info))
-            cache.expire(ticker, redis_ttl)
-            info = stock.info
-            # info['x-cache'] = False
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
-    return jsonify(info)
+@app.route('/versions')
+def versions():
+    packages = {
+        "Flask": pkg_resources.get_distribution("Flask").version,
+        "Werkzeug": pkg_resources.get_distribution("Werkzeug").version,
+        "yfinance": pkg_resources.get_distribution("yfinance").version,
+        "gunicorn": pkg_resources.get_distribution("gunicorn").version
+    }
+    return jsonify(packages)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
