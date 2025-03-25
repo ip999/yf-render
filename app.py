821a2cec23b3e26a686977f5e9002ff408619486
I P <porteous@gmail.com>
2025-03-25 21:18:32 +0000

    add ttl to response

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import pkg_resources
 import redis
 import os
 
-
 app = Flask(__name__)
 
 # Load Redis configuration from environment variables
@@ -39,15 +38,20 @@ def cached():
     ticker = request.args.get('ticker')
     cached_info = cache.get(ticker)
     if cached_info is not None:
+        # Get the remaining TTL for the cached key
+        remaining_ttl = cache.ttl(ticker)
+        
         info = json.loads(cached_info)
-        info['x-cache'] = "hit"
+        info['x-cache'] = True
+        info['x-cache-ttl'] = remaining_ttl
     else:
         try: 
             stock = yf.Ticker(ticker)
             cache.set(ticker, json.dumps(stock.info))
             cache.expire(ticker, redis_ttl)
             info = stock.info
-            info['x-cache'] = "miss"
+            info['x-cache'] = False
+            info['x-cache-ttl'] = redis_ttl
         except Exception as e:
             return jsonify({'error': str(e)}), 500
     return jsonify(info)
